7f7096f048ad7f9dbbd95bc97194b50ced7ae611
Guanyu He <ghe@linkedin.com>
2022-05-17 09:07:13 -0700

    make sure active key works on both local dev and GH pages

--- webui/src/TopNavigationBar.js
+++ webui/src/TopNavigationBar.js
@@ -5,12 +5,15 @@ function TopNavigationBar() {
     if (window.location.origin.indexOf(".github.io") >= 0) {
         slash = "#";
     }
-    console.log(slash);
+    let activeKey = window.location.href.split("/").pop();
+    if (slash == "/") {
+        activeKey = "/" + activeKey;
+    }
     return (
         <div className={"topNavigationBar"}>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Nav className="ml-auto" activeKey={window.location.href.replace(window.location.origin+"/","")}>
+                    <Nav className="ml-auto" activeKey={activeKey}>
                         <Navbar.Brand href={slash}>Test Github CI</Navbar.Brand>
                         <Nav.Link href={slash + "dashboards"} >Dashboards</Nav.Link>
                         <Nav.Link href={slash + "getstarted"} >Get Started</Nav.Link>
